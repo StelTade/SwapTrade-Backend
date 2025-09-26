0af9b4dda6135b0fc6f1fd9894a94cab0aff8081
Akshola00 <akinniyishola07@gmail.com>
2025-09-26 17:54:55 +0100

    Create placeholder directories for each module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { TradingModule } from './trading/trading.module';
+import { UserModule } from './user/user.module';
+import { RewardsModule } from './rewards/rewards.module';
+import { NotificationModule } from './notification/notification.module';
+import { BiddingModule } from './bidding/bidding.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PortfolioModule, TradingModule, UserModule, RewardsModule, NotificationModule, BiddingModule, CommonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
